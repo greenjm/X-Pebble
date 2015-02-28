3d69f3325b6f39223976b85ecfa00d73f8815419
unknown <greenjm@GREENJM-1.rose-hulman.edu>
2015-02-28 07:45:48 -0500

    testing

--- javascript/workout.js
+++ javascript/workout.js
@@ -7,8 +7,11 @@ var populateDatalist = function(){
 	console.log(datalist);
 	$.ajax({
 		url: "../scripts/get_all_exercises_by_name.php",
+		datatype: "json";
 		success: function(data){
-			console.log(data['1']['name']);
+			for(var id in data){
+				console.log(data['1']['name']);
+			}	
 			document.createElement('option');
 
 			console.log(data);
