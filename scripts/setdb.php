058745baa03dfedb53eed3509b6b427b9b9685f3
unknown <greenjm@GREENJM-1.rose-hulman.edu>
2015-02-28 05:20:04 -0500

    Added script for connecting to database

---
+++ scripts/setdb.php
@@ -0,0 +1,10 @@
+<?php
+try{
+  	$db = new PDO ("sqlsrv:server = tcp:pqaeo749p6.database.windows.net,1433; Database = XPebbleDatabase", "greenjm", "AW#zse4xdr5");
+  	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  	echo "success";
+} catch ( PDOException $e ) { 
+    print("Error connecting to SQL Server.");
+  	die(print_r($e));
+  }
+?>
