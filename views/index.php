129c898d584204ff25bbed79f7365dd0477262ad
IanHavens <havensid@rose-hulman.edu>
2015-02-28 07:00:15 -0500

    login

--- views/index.php
+++ views/index.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Home Page</title>
 	<link rel="stylesheet" href="../styles/main.css">
-	<link rel="stylesheet" href="../styles/profile.css">
+	<link rel="stylesheet" href="../styles/index.css">
 	<script src="../libraries/jquery-2.1.3.min.js"></script>
 
 	<link href="../images/fav.gif" type="image/gif" rel="shortcut icon"/>
@@ -13,12 +13,20 @@
 <body>
 	<?PHP include 'navbar.html'; ?>
 	<div class="wrapper">
-		<div class="center">
-			
-		<div style="display:none;" id="blackBackground" onclick="closeViewer()"></div>
+		<div id="log-wrapper">
+			<div id="loginHeader">
+				<h1 align="center">Login</h1>
+			</div>
+			<div id="logForm" align="center">
+				<label for="username"><h2>Username:</h2></label>
+				<input type="text" name="username" autofocus />
+				<br>
+				<br>
+				<label for="password"><h2>Password:</h2></label>
+				<input type="password" name="password" autofocus/>
+				<br>
+			</div>
+		</div>
 	</div>
-	<script type="text/javascript">
-		buildPage("");
-	</script>
 </body>
 </html>
