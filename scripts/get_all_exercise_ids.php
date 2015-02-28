b279759b52c3c8774bf1b7b752540eac306e246c
unknown <greenjm@GREENJM-1.rose-hulman.edu>
2015-02-28 17:41:55 -0500

    Fixed get_all_exercise_ids.php

--- scripts/get_all_exercise_ids.php
+++ scripts/get_all_exercise_ids.php
@@ -3,7 +3,14 @@
 
 	include 'setdb.php';
 
-	$query = $db->prepare('select id from exercise order by id asc');
+	$pebbleid = $_GET['pebbleid']
+
+	$query = $db->prepare('select e.id from exercise e
+	join routine r on r.exercise = e.name
+	join workout w on w.workid = r.workid
+	join users u on u.userid = w.userid
+	where u.pebbleid=:pebbleid');
+	$query->bindValue(':pebbleid', $pebbleid, PDO::PARAM_STR);
 	$query->execute();
 
 	$results = array();
