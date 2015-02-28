55be1f93964d1b735b64b506aff693fe80c844a3
IanHavens <havensid@rose-hulman.edu>
2015-02-28 11:08:43 -0500

    filling in tabs

--- views/exercises.php
+++ views/exercises.php
@@ -16,31 +16,111 @@
 	<div class="wrapper">
 		<div class="tabs">
 			<ul class="tab-links">
-				<li class="active"><a href="#tab1">Tab #1</a></li>
-				<li><a href="#tab2">Tab #2</a></li>
-				<li><a href="#tab3">Tab #3</a></li>
-				<li><a href="#tab4">Tab #4</a></li>
+				<li class="active"><a href="#aero">Aerobic</a></li>
+				<li><a href="#anaer">Anaerobic</a></li>
+				<li><a href="#yoga">Yoga</a></li>
+				<li><a href="#stre">Stretching</a></li>
 			</ul>
  
 			<div class="tab-content">
-				<div id="tab1" class="tab active">
-					<p>Tab #1 content goes here!</p>
-					<p>Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum risus ornare mollis.</p>
+				<div id="aero" class="tab active">
+					<table border="1">
+						<tr>
+							<td>Push-Ups</td>
+							<td>Lay on ground prone with hands under shoulders. Keep a straight back and hold the head up. Push body up all the way without locking the elbows. Then lower body down until elbows are at a 90 degree angle. Repeat. Keep a steady pace on both the descent and ascent. Duration: 60 seconds</td>
+							<td><img alt="pushups" src="../images/PushUps B.png"></img></td>
+						</tr>
+						<tr>
+							<td>Plank</td>
+							<td>Get into pushup position on the floor. Now bend your elbows 90 degrees and rest your weight on your forearms. Your elbows should be directly beneath your shoulders, and your body should form a straight line from your head to your feet. Duration: 120 seconds</td>
+							<td>pics</td>
+						</tr>
+						<tr>
+							<td>Crunches</td>
+							<td>Lay on your back with your hands behind your head and your legs bent. Lift your torso and </td>
+							<td>pics</td>
+						</tr>
+						<tr>
+							<td>Squats</td>
+							<td>Stand with your feet approximately shoulder width apart. Put your hands on your head. Squat down while keeping your back straight and your knees above your feet. Duration: 60 seconds</td>
+							<td>pics</td>
+						</tr>
+					</table>
 				</div>
  
-				<div id="tab2" class="tab">
-					<p>Tab #2 content goes here!</p>
-					<p>Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum risus ornare mollis. In hac habitasse platea dictumst. Ut euismod tempus hendrerit. Morbi ut adipiscing nisi. Etiam rutrum sodales gravida! Aliquam tellus orci, iaculis vel.</p>
+				<div id="anaer" class="tab">
+					<table border="1">
+						<tr>
+							<td>Bench Press</td>
+							<td>Instructions</td>
+							<td>pics</td>
+						</tr>
+						<tr>
+							<td>Lat. Pull Downs</td>
+							<td>Instructions</td>
+							<td>pics</td>
+						</tr>
+						<tr>
+							<td>Tricep Push Downs</td>
+							<td>Instructions</td>
+							<td>pics</td>
+						</tr>
+						<tr>
+							<td>Bicep Curls</td>
+							<td>Instructions</td>
+							<td>pics</td>
+						</tr>
+					</table>
 				</div>
  
-				<div id="tab3" class="tab">
-					<p>Tab #3 content goes here!</p>
-					<p>Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum ri.</p>
+				<div id="yoga" class="tab">
+					<table border="1">
+						<tr>
+							<td>Tree Pose</td>
+							<td>Instructions</td>
+							<td>pics</td>
+						</tr>
+						<tr>
+							<td>Downward Dog</td>
+							<td>Instructions</td>
+							<td>pics</td>
+						</tr>
+						<tr>
+							<td>Upward Dog</td>
+							<td>Instructions</td>
+							<td>pics</td>
+						</tr>
+						<tr>
+							<td>Warrior Pose</td>
+							<td>Instructions</td>
+							<td>pics</td>
+						</tr>
+					</table>
 				</div>
  
-				<div id="tab4" class="tab">
-					<p>Tab #4 content goes here!</p>
-					<p>Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum risus ornare mollis. In hac habitasse platea dictumst. Ut euismod tempus hendrerit. Morbi ut adipiscing nisi. Etiam rutrum sodales gravida! Aliquam tellus orci, iaculis vel.</p>
+				<div id="stre" class="tab">
+					<table border="1">
+						<tr>
+							<td>Toe Touch</td>
+							<td>Instructions</td>
+							<td>pics</td>
+						</tr>
+						<tr>
+							<td>Side Lunge</td>
+							<td>Instructions</td>
+							<td>pics</td>
+						</tr>
+						<tr>
+							<td>Butterfly</td>
+							<td>Instructions</td>
+							<td>pics</td>
+						</tr>
+						<tr>
+							<td>Arm-bar</td>
+							<td>Instructions</td>
+							<td>pics</td>
+						</tr>
+					</table>
 				</div>
 			</div>
 		</div>
