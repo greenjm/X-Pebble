1263428b2edc40034e7678a807880326a1e3baed
unknown <greenjm@GREENJM-1.rose-hulman.edu>
2015-02-28 07:11:47 -0500

    more tests

--- scripts/setdb.php
+++ scripts/setdb.php
@@ -1,6 +1,6 @@
 <?php
 try{
-  	$db = new PDO ("sqlsrv:server = tcp:pqaeo749p6.database.windows.net; Database = XPebbleDatabase", "greenjm", "AW#zse4xdr5");
+  	$db = new PDO ("sqlsrv:server = tcp:pqaeo749p6.database.windows.net;port=1433; Database = XPebbleDatabase", "greenjm", "AW#zse4xdr5");
   	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 } catch ( PDOException $e ) { 
     print("Error connecting to SQL Server.");
