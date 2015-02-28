54f18aa0c104688f6fd49b10005eacde7ad2e6e1
unknown <greenjm@GREENJM-1.rose-hulman.edu>
2015-02-28 10:50:38 -0500

    duration script should work now

--- scripts/get_exercise_duration.php
+++ scripts/get_exercise_duration.php
@@ -4,17 +4,12 @@ include "setdb.php";
 
 $name = $_GET['name'];
 
-echo "starting";
-
 $query = $db->prepare('select duration from exercise where name=:name');
 $query->bindValue(':name', $name, PDO::PARAM_STR);
 $query->execute();
 
-echo "success";
-return;
+$row = $query->fetch(PDO::FETCH_ASSOC);
 
-$rowCount = $query->rowCount();
-
-echo $rowCount;
+echo $row['duration'];
 
 ?>
