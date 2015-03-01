2f08a2c69d728013eb4ccd549987f5e0b87c0102
MaKn1 <kniermmf@rose-hulman.edu>
2015-02-28 23:39:01 -0500

    ...

--- views/exercises.php
+++ views/exercises.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Home Page</title>
+	<title>Exercise Information</title>
 	<link rel="stylesheet" href="../styles/main.css">
 	<link rel="stylesheet" href="../styles/exercises.css">
 	<script src="../libraries/jquery-2.1.3.min.js"></script>
