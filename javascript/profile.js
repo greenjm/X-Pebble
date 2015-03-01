99626cd718ae6065c45030612bb4f8e48454ba8c
unknown <greenjm@GREENJM-1.rose-hulman.edu>
2015-02-28 22:30:18 -0500

    Should be filling table for first tab

--- javascript/profile.js
+++ javascript/profile.js
@@ -21,10 +21,29 @@ window.onload = function(){
 }
 
 var get_stats = function(category){
+	var id;
+	if (category == 1){
+		id = "aero";
+	}else if (category == 2){
+		id = "yoga";
+	}else if (category == 3){
+		id = "anaer";
+	} else {
+		id = "stre";
+	}
+	console.log(id);
+	var completed = $("#" + id + " table tr:first-of-type td:nth-of-type(2)");
+	var attempted = $("#" + id + " table tr:nth-of-type(2) td:nth-of-type(2)");
+	var percentCompletion = $("#" + id + " table tr:nth-of-type(3) td:nth-of-type(2)");
+	var popularity = $("#" + id + " table tr:nth-of-type(4) td:nth-of-type(2)");
+	/*var completed = document.getElementById('' + id);
+	completed = completed.firstChild;*/
+	console.log(completed);
 	var packet = {
 		"category": category,
 		"userid": USERID
 	};
+	var results;
 	console.log(USERID);
 	$.ajax ({
 		type: "GET",
@@ -32,8 +51,12 @@ var get_stats = function(category){
 		data: packet,
 		dataType: "json",
 		success: function(data){
-			console.log(data);
+			results = data;
+			completed.html(data[1]);
+			attempted.html(data[0]);
+			percentCompletion.html(data[1] / data[0]);
+			popularity.html(data[0] / data[2]);
 		}
-	})
+	});
 }
 	
